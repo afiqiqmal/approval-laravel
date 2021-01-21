5c3424d562f024e01889f8a5e2d9ccce7846041d
Hafiq <hafiqiqmal93@gmail.com>
2021-01-21 23:12:49 +0800

    Fix Config

--- config/approval.php
+++ config/approval.php
@@ -4,6 +4,12 @@ return [
     'enabled' => env('APPROVAL_ENABLED', true),
 
     'model' => [
+        'reject_by' => \App\Models\User::class,
+        'approve_by' => \App\Models\User::class,
+    ],
+
+    'tables' => [
+        'name' => 'approvals',
         'reject_by' => 'users',
         'approve_by' => 'users',
     ],
