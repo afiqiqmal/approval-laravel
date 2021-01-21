4ba22acfff458a794d4f5a330f068d4a80481928
Hafiq <hafiqiqmal93@gmail.com>
2021-01-21 23:51:20 +0800

    Update Fix

--- src/Models/Traits/HasApprovable.php
+++ src/Models/Traits/HasApprovable.php
@@ -9,23 +9,53 @@ trait HasApprovable
     {
     }
 
-    public function canMakeApproval()
+    /**
+     * @return bool
+     */
+    public function canMakeApproval(): bool
     {
         return true;
     }
 
-    public function canMakeReject()
+    /**
+     * @return bool
+     */
+    public function canMakeReject(): bool
     {
         return true;
     }
 
-    public function hasFullApprovalPermission()
+    /**
+     * @return bool
+     */
+    public function hasFullApprovalPermission(): bool
     {
-        return config('approval.enabled') && ($this->canMakeApproval() && $this->canMakeReject());
+        return $this->canMakeApproval() && $this->canMakeReject();
     }
 
-    public function canMakeApprovalOrReject()
+    /**
+     * @return bool
+     */
+    public function canMakeApprovalOrReject(): bool
     {
-        return config('approval.enabled') && ($this->canMakeApproval() || $this->canMakeReject());
+        return$this->canMakeApproval() || $this->canMakeReject();
+    }
+
+    /**
+     * @param $route1
+     * @param $route2
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectRouteBasedOnPermission($route1, $route2): \Illuminate\Http\RedirectResponse
+    {
+        if (!config('approval.enabled')) {
+            return redirect()->route($route1);
+        }
+
+        if ($this->hasFullApprovalPermission()) {
+            return redirect()->route($route1);
+        }
+
+        return redirect()->route($route2);
     }
 }
